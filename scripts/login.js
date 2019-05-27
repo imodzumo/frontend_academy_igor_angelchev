d56d74e8163b4f5048382ac4de9eb096be925c8b
Igor Angelchev <angel4ev2@gmail.com>
2019-05-28 01:14:45 +0300

    Added products page and filter js

--- scripts/login.js
+++ scripts/login.js
@@ -1,10 +1,14 @@
 var login = document.getElementById('login');
 var loginBar = document.getElementById('login_bar');
 var curtain = document.getElementById('curtain');
+var close = document.getElementById('close');
 
 login.addEventListener('click',function () {
     loginBar.classList.remove('hidden');
 },false);
 curtain.addEventListener('click', function () {
+    loginBar.classList.add('hidden');
+},false);
+close.addEventListener('click', function () {
     loginBar.classList.add('hidden');
 },false);
---
+++ scripts/shop_filter.js
@@ -0,0 +1,73 @@
+var brands = document.getElementById('brands');
+var brands_menu = document.getElementById('brands_menu');
+var brands_menu_curtain = document.getElementById('brands_menu_curtain');
+
+var categories = document.getElementById('categories');
+var categories_menu = document.getElementById('categories_menu');
+var categories_menu_curtain = document.getElementById('categories_menu_curtain');
+
+var price = document.getElementById('price');
+var price_menu = document.getElementById('price_menu');
+var price_menu_curtain = document.getElementById('price_menu_curtain');
+
+var li_brands = document.querySelectorAll('.shop-filter__main__wrapper__box__bar__subMenu__brands > .shop-filter__main__wrapper__box__bar__subMenu__li');
+var li_categories = document.querySelectorAll('.shop-filter__main__wrapper__box__bar__subMenu__categories > .shop-filter__main__wrapper__box__bar__subMenu__li');
+var li_price = document.querySelectorAll('.shop-filter__main__wrapper__box__bar__subMenu__price > .shop-filter__main__wrapper__box__bar__subMenu__li');
+
+
+/*Open menu*/
+brands.addEventListener('click',function () {
+    brands_menu.classList.remove('hidden');
+    brands_menu_curtain.classList.remove('hidden');
+
+    /*Choose filter*/
+    li_brands.forEach(function(e) {
+            e.addEventListener('click', function () {
+                console.log(brands.textContent = e.textContent);
+            }, false);
+        }
+    );
+},false);
+
+categories.addEventListener('click',function () {
+    categories_menu.classList.remove('hidden');
+    categories_menu_curtain.classList.remove('hidden');
+
+    /*Choose filter*/
+    li_categories.forEach(function(e) {
+            e.addEventListener('click', function () {
+                console.log(categories.textContent = e.textContent);
+            }, false);
+        }
+    );
+},false);
+
+price.addEventListener('click',function () {
+    price_menu.classList.remove('hidden');
+    price_menu_curtain.classList.remove('hidden');
+
+    /*Choose filter*/
+    li_price.forEach(function(e) {
+            e.addEventListener('click', function () {
+                console.log(price.textContent = e.textContent);
+            }, false);
+        }
+    );
+},false);
+
+/*Close menu*/
+brands_menu_curtain.addEventListener('click',function () {
+    brands_menu.classList.add('hidden');
+    brands_menu_curtain.classList.add('hidden');
+},false);
+
+categories_menu_curtain.addEventListener('click',function () {
+    categories_menu.classList.add('hidden');
+    categories_menu_curtain.classList.add('hidden');
+},false);
+
+price_menu_curtain.addEventListener('click',function () {
+    price_menu.classList.add('hidden');
+   price_menu_curtain.classList.add('hidden');
+},false);
+
